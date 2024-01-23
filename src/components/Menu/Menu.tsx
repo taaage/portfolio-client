a2e2cb2b2c7ad69d420ea14186545fa84829e5ef
Tigge <tigge.nilsson@ericsson.com>
2024-01-23 22:08:24 +0100

    refactor: flex rules to menu and home

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -10,27 +10,23 @@ const Menu = () => {
     <div className="menu-container">
       <div className="menu-item" onClick={() => navigate("/")}>
         <p className="menu-item-text">
-          <span className="menu-item-number">01. </span>Home
+          <span className="menu-item-number">1 </span>Home
         </p>
       </div>
-      <div className="menu-item" onClick={() => navigate("/about")}>
-        <p className="menu-item-text">
-          <span className="menu-item-number">02. </span>About
-        </p>
-      </div>
+
       <div className="menu-item" onClick={() => navigate("/projects")}>
         <p className="menu-item-text">
-          <span className="menu-item-number">03. </span>Projects
+          <span className="menu-item-number">3 </span>Projects
         </p>
       </div>
       <div className="menu-item" onClick={() => navigate("/contact")}>
         <p className="menu-item-text">
-          <span className="menu-item-number">04. </span>Contact
+          <span className="menu-item-number">3 </span>Contact
         </p>
       </div>
-      <div className="menu-item" onClick={() => navigate("/cv")}>
+      <div className="menu-item" onClick={() => navigate("/about")}>
         <p className="menu-item-text">
-          <span className="menu-item-number">05. </span>CV
+          <span className="menu-item-number">7 </span>About
         </p>
       </div>
     </div>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "../../components/Header/Header";
 
 import "../../App.css";
@@ -9,22 +8,39 @@ const Home = () => {
     <>
       <Header />
       <div className="home-content-container">
-        <div className="home-content-row-container-one">
+        <div>
           <p className="home-content-row-one">Hello, my name is</p>
         </div>
-        <div className="home-content-row-container-two">
+        <div>
           <p className="home-content-row-two">Tigge Nilsson.</p>
         </div>
-        <div className="home-content-row-container-three">
+        <div>
           <p className="home-content-row-three">I build things for the web.</p>
         </div>
-        <div className="home-content-row-container-four">
+        <div>
           <p className="home-content-row-four">
-            I'm a software engineer specializing in React building exceptional
+            I'm a software engineer specializing in React, building exceptional
             digital experiences. Currently I'm focused on building accessable
             and human friendly products at{" "}
-            <span className="green-color">Ericsson</span> in Stockholm.
+            <a
+              className="ericsson-text green-color"
+              href="https://www.ericsson.com/"
+              target="_blank "
+              rel="noreferrer"
+            >
+              Ericsson.
+            </a>
           </p>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              window.open("https://github.com", "_blank");
+            }}
+            className="github-button"
+          >
+            GitHub
+          </button>
         </div>
       </div>
     </>
